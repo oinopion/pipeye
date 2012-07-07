f7853b388fc70f2608aaab282dabe162ccc2eddc
Tomek Paczkowski <tomek@hauru.eu>
2012-07-08 01:57:35 +0200

    Fixes tests

--- pipeye/tests.py
+++ pipeye/tests.py
@@ -1,5 +1,15 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 class HomeViewTest(TestCase):
-    def test_redirects(self):
-        assert False
+    def get(self):
+        url = reverse('home')
+        return self.client.get(url)
+
+    def test_renders_template(self):
+        resp = self.get()
+        self.assertTemplateUsed(resp, 'home.html')
+
+    def test_response_is_ok(self):
+        resp = self.get()
+        self.assertEqual(200, resp.status_code)
