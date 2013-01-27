f3fb70df4dfc3b6ee5cdce925afdf6d02cc19bde
Tomek Paczkowski <tomek@hauru.eu>
2013-01-27 16:25:37 +0100

    Styling modification

--- pipeye/packages/tests/factories.py
+++ pipeye/packages/tests/factories.py
@@ -1,11 +1,11 @@
-from factory import Factory, Sequence
+import factory
 from ..models import PackageRelease
 
 version_gen = lambda n: u'1.%s' % n
 
-class PackageReleaseFactory(Factory):
+class PackageReleaseFactory(factory.Factory):
     FACTORY_FOR = PackageRelease
-    version = Sequence(version_gen)
+    version = factory.Sequence(version_gen)
     summary = u'A package'
     home_page = u'http://example.com'
     package_url = u'http://example.com'
