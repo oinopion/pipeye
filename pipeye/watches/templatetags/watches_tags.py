341f97d734b66669c4cb08ec23b7baa7c3c548c0
Tomek Paczkowski <tomek@hauru.eu>
2013-02-02 15:56:47 +0100

    Display unwatch button on package page

--- pipeye/watches/templatetags/watches_tags.py
+++ pipeye/watches/templatetags/watches_tags.py
@@ -6,5 +6,9 @@ register = template.Library()
 
 @register.inclusion_tag('watches/_watch_button.html', takes_context=True)
 def watch_button(context, package):
-    action, text = url('create_watch', package), 'Watch this package'
+    user = context['user']
+    if package.watch_set.filter(user=user).exists():
+        action, text = url('delete_watch', package), 'Stop watching this package'
+    else:
+        action, text = url('create_watch', package), 'Watch this package'
     return {'action': action, 'text': text}
---
+++ pipeye/watches/tests/test_templatetags.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django.template import Template, Context
+from expecter import expect
+from pipeye.packages.tests.factories import PackageFactory
+from pipeye.utils.factories import UserFactory
+from pipeye.watches.tests.factories import WatchFactory
+
+class WatchButtonTagTest(TestCase):
+    TAG_HTML = "{% load watches_tags %}{% watch_button package %}"
+
+    def render(self, context):
+        template = Template(self.TAG_HTML)
+        return template.render(Context(context))
+
+    def test_displays_watch_button(self):
+        package = PackageFactory.create()
+        user = UserFactory.create()
+        html = self.render({'user': user, 'package': package})
+        expect(html).contains('Watch this package')
+
+    def test_displays_unwatch_button(self):
+        watch = WatchFactory.create()
+        html = self.render({'user': watch.user, 'package': watch.package})
+        expect(html).contains('Stop watching this package')
