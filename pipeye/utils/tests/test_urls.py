2ff6fbfdf7ef1ccd4beca5b0b582f7c6fbcf1158
Tomek Paczkowski <tomek@hauru.eu>
2013-02-28 17:39:15 +0100

    Refactored url tests

--- pipeye/utils/tests/test_urls.py
+++ pipeye/utils/tests/test_urls.py
@@ -4,33 +4,35 @@ from mock import patch
 from ..urls import url, url_lazy
 
 
-@patch('pipeye.utils.urls.urlresolvers')
-class UrlTest(TestCase):
+class BaseUrlTest(object):
     args = ('a1', 'a2')
     kwargs = {'kw1': 'kwarg1', 'kw2': 'kwarg2'}
 
-    def test_passes_args_and_kwargs(self, resolvers):
+    def setUp(self):
+        self.patch = patch('pipeye.utils.urls.urlresolvers')
+        self.resolvers = self.patch.start()
+
+    def tearDown(self):
+        self.patch.stop()
+
+
+class UrlTest(BaseUrlTest, TestCase):
+    def test_passes_args_and_kwargs(self):
         url('view_name', *self.args, **self.kwargs)
-        resolvers.reverse.assert_called_with('view_name',
+        self.resolvers.reverse.assert_called_with('view_name',
                 args=self.args, kwargs=self.kwargs)
 
-    def test_returns_result(self, resolvers):
+    def test_returns_result(self):
         result = url('view_name')
-        expect(result) == resolvers.reverse.return_value
+        expect(result) == self.resolvers.reverse.return_value
 
 
-@patch('pipeye.utils.urls.urlresolvers')
-class LazyUrlTest(TestCase):
-    args = ('a1', 'a2')
-    kwargs = {'kw1': 'kwarg1', 'kw2': 'kwarg2'}
-
-    def test_passes_args_and_kwargs(self, resolvers):
+class LazyUrlTest(BaseUrlTest, TestCase):
+    def test_passes_args_and_kwargs(self):
         url_lazy('view_name', *self.args, **self.kwargs)
-        resolvers.reverse_lazy.assert_called_with('view_name',
+        self.resolvers.reverse_lazy.assert_called_with('view_name',
                 args=self.args, kwargs=self.kwargs)
 
-    def test_returns_result(self, resolvers):
+    def test_returns_result(self):
         result = url_lazy('view_name')
-        expect(result) == resolvers.reverse_lazy.return_value
-
-
+        expect(result) == self.resolvers.reverse_lazy.return_value
