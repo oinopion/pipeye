3cd40f9672e7ce8a9c17c46805d4ce45f55a127f
Tomek Paczkowski <tomek@hauru.eu>
2013-02-28 18:06:59 +0100

    Added qs_url function

--- pipeye/utils/tests/test_urls.py
+++ pipeye/utils/tests/test_urls.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from expecter import expect
 from mock import patch
-from ..urls import url, url_lazy
+from ..urls import url, url_lazy, qs_url
 
 
 class BaseUrlTest(object):
@@ -36,3 +36,22 @@ class LazyUrlTest(BaseUrlTest, TestCase):
     def test_returns_result(self):
         result = url_lazy('view_name')
         expect(result) == self.resolvers.reverse_lazy.return_value
+
+
+class QsUrlTest(BaseUrlTest, TestCase):
+    def setUp(self):
+        super(QsUrlTest, self).setUp()
+        self.resolvers.reverse.return_value = '/path/'
+
+    def test_adds_qs_to_url(self):
+        result = qs_url('view', qs='a=b')
+        expect(result) == '/path/?a=b'
+
+    def test_underscore_qs_substitute(self):
+        qs_url('view', qs='kwarg', _qs='a=b')
+        self.resolvers.reverse.assert_called_with('view', args=(),
+                kwargs={'qs': 'kwarg'})
+
+    def test_qs_as_dict(self):
+        result = qs_url('view', qs={'a': 'b'})
+        expect(result) == '/path/?a=b'
--- pipeye/utils/urls.py
+++ pipeye/utils/urls.py
@@ -1,9 +1,21 @@
+from urllib import urlencode
 from django.core import urlresolvers
 
 
-def url(viewname, *args, **kwargs):
-    return urlresolvers.reverse(viewname, args=args, kwargs=kwargs)
+def url(view_name, *args, **kwargs):
+    return urlresolvers.reverse(view_name, args=args, kwargs=kwargs)
 
 
-def url_lazy(viewname, *args, **kwargs):
-    return urlresolvers.reverse_lazy(viewname, args=args, kwargs=kwargs)
+def url_lazy(view_name, *args, **kwargs):
+    return urlresolvers.reverse_lazy(view_name, args=args, kwargs=kwargs)
+
+
+def qs_url(view_name, *args, **kwargs):
+    if '_qs' in kwargs:
+        qs = kwargs.pop('_qs')
+    else:
+        qs = kwargs.pop('qs', '')
+    if isinstance(qs, dict):
+        qs = urlencode(qs)
+    path = url(view_name, *args, **kwargs)
+    return "%s?%s" % (path, qs)
