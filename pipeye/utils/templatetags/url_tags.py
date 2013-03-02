5951c6ca40b3dbc305a4aaee3390ff49101e8e41
Tomek Paczkowski <tomek@hauru.eu>
2013-03-02 11:05:20 +0100

    Adds escaping to qs templatetag

--- pipeye/utils/templatetags/url_tags.py
+++ pipeye/utils/templatetags/url_tags.py
@@ -1,15 +1,18 @@
 from urllib import urlencode
 from django import template
+from django.utils.html import escape
 
 register = template.Library()
 
-@register.simple_tag
-def qs(path=None, defaults=None, **query_params):
+@register.simple_tag(takes_context=True)
+def qs(context, path=None, defaults=None, **query_params):
     if isinstance(path, dict):
         path, defaults = None, path
     if defaults:
         query_params = dict(defaults, **query_params)
-    query_string = urlencode(query_params)
+    qs_url = urlencode(query_params)
     if path:
-        return "%s?%s" % (path, query_string)
-    return query_string
+        qs_url = "%s?%s" % (path, qs_url)
+    if context.autoescape:
+        qs_url = escape(qs_url)
+    return qs_url
